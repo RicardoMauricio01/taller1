2a7eb4fefdef57caf6dd561fc3618c308d7341d5
ricardomauriciodev <ricardomauricio.dev@gmail.com>
2026-03-22 23:35:13 -0300

    Probando

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
 
+// en password usar el password que usas para tu base de datos postgres
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "chatdb",
-  password: "root", // cambia por tu contraseña
+  password: "root", 
   port: 5432,
 });
 
